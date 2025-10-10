2223b2bac9b575b23dcd4713723ac45b28c9724e
Karol Bystrek <karbystrek@gmail.com>
2025-10-10 14:07:19 +0200

    Initial commit

---
+++ src/main/java/com/karolbystrek/tennispredictor/TennisPredictorApplication.java
@@ -0,0 +1,13 @@
+package com.karolbystrek.tennispredictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisPredictorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TennisPredictorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/karolbystrek/tennispredictor/TennisPredictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karolbystrek.tennispredictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisPredictorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
